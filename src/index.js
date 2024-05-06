7bf50d27d20df84e71e2a8a09f9b039c2c374893
Niha Mukhtar Wani <niha78781@gmail.com>
2024-05-06 20:20:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ import "./styles.css";
 const ran = "https://picsum.photos/200";
 ReactDom.render(
   <div>
-    <h1 className="try">Hello</h1>,
-    <ul>
-      <li>TOC</li>
-    </ul>
-    ,
-    <div>
-      <img src="https://www.sharmispassions.com/wp-content/uploads/2014/11/SchezwanFriedRice5.jpg" />
-      <img src="https://i.ytimg.com/vi/CIVBFsEyViQ/maxresdefault.jpg" />
-      <img src="https://lh3.googleusercontent.com/proxy/XWraYwonqMSUjIWytmToMWv6FmOogSyEJIE4xGKa3n-fctdn7ejfqO0DsbGWp_WXastzvTunokYXmzGh8tBmeyRDpXb5jaW56vvE3ExQ9CY3EOm4SBtH437VnNW05yzARkLYRQ" />
-      <img src={ran + "?grayscale"} />
-    </div>
-  </div>,
+      <h1 className="try">Hello</h1>,
+      <ul>
+          <li>TOC</li>
+       </ul>,
+       <div>
+            <img src="https://www.sharmispassions.com/wp-content/uploads/2014/11/SchezwanFriedRice5.jpg" />
+            <img src="https://i.ytimg.com/vi/CIVBFsEyViQ/maxresdefault.jpg" />
+            <img src="https://lh3.googleusercontent.com/proxy/XWraYwonqMSUjIWytmToMWv6FmOogSyEJIE4xGKa3n-fctdn7ejfqO0DsbGWp_WXastzvTunokYXmzGh8tBmeyRDpXb5jaW56vvE3ExQ9CY3EOm4SBtH437VnNW05yzARkLYRQ" />
+            <img src={ran + "?grayscale"} />
+        </div>
+   </div>,
   document.getElementById("root")
 );
